857b73b539d04091b101158acf366d3e479f3751
Barla Ankit <barla.x88@gmail.com>
2024-01-23 15:54:17 +0530

    Add slide data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import './App.css';
+import { data } from './data';
+
 function App() {
   return (
-    <>
-      <h1>React slider</h1>
-    </>
+    <div className="slide-container">
+      {data.map((person, index) => {
+        return (
+          <div className="slide" key={index}>
+            <h1>{person.name}</h1>
+            <p>{person.text}</p>
+          </div>
+        );
+      })}
+    </div>
   );
 }
 
