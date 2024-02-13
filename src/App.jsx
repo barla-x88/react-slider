4a107a8e81ab728b79822063d7a52dad9bd5f3c3
Barla Ankit <barla.x88@gmail.com>
2024-02-13 12:34:50 +0530

    Change slider data and add autoplay

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
-import { data } from './data';
+import { linuxDistros } from './data';
+import { AiFillCaretLeft } from 'react-icons/ai';
+import { AiFillCaretRight } from 'react-icons/ai';
 
 function App() {
   const [activeSlide, setActiveSlide] = useState(0);
 
+  useEffect(() => {
+    const autoSlide = setInterval(() => {
+      nextSlide();
+    }, 5000);
+
+    return () => {
+      clearInterval(autoSlide);
+    };
+  }, [activeSlide]);
+
   const prevSlide = () => {
     setActiveSlide((oldSlide) => {
-      return oldSlide === 0 ? data.length - 1 : oldSlide - 1;
+      return oldSlide === 0 ? linuxDistros.length - 1 : oldSlide - 1;
     });
   };
 
   const nextSlide = () => {
     setActiveSlide((oldSlide) => {
-      return oldSlide + 1 < data.length ? oldSlide + 1 : 0;
+      return oldSlide + 1 < linuxDistros.length ? oldSlide + 1 : 0;
     });
   };
   return (
     <div className="slide-container">
-      {data.map((person, index) => {
+      <h1>Popular Linux Distros</h1>
+      {linuxDistros.map((distro, index) => {
+        const {
+          distroName,
+          description,
+          firstRelease,
+          currentVersion,
+          distroLogo,
+          officialWebsite,
+        } = distro;
         return (
           <div
             className="slide"
@@ -27,16 +48,24 @@ function App() {
               transform: `translate(${(index - activeSlide) * 100}%`,
             }}
           >
-            <h1>{person.name}</h1>
-            <p>{person.text}</p>
+            <div className="img">
+              <img src={distroLogo} alt={distroName} />
+            </div>
+            <h2>{distroName}</h2>
+            <p>{description}</p>
+            <p>First Release - {firstRelease}</p>
+            <p>Current Version - {currentVersion}</p>
+            <a href={officialWebsite} className="download-btn">
+              Download linuxDistros
+            </a>
           </div>
         );
       })}
       <button className="prev btn" onClick={prevSlide}>
-        prev
+        <AiFillCaretLeft />
       </button>
       <button className="next btn" onClick={nextSlide}>
-        next
+        <AiFillCaretRight />
       </button>
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -1,20 +1,53 @@
-const data = [
+const linuxDistros = [
   {
-    name: 'henry',
-    text: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
+    distroName: 'Ubuntu',
+    description:
+      'A popular Debian-based Linux distribution known for its ease of use and community support.',
+    firstRelease: 'October 20, 2004',
+    currentVersion: '22.04 LTS (Jammy Jellyfish)',
+    distroLogo:
+      'https://upload.wikimedia.org/wikipedia/commons/a/ab/Logo-ubuntu_cof-orange-hex.svg',
+    officialWebsite: 'https://ubuntu.com/',
   },
   {
-    name: 'Helen',
-    text: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
+    distroName: 'Fedora',
+    description:
+      'A community-supported Linux distribution sponsored by Red Hat, known for its focus on innovation.',
+    firstRelease: 'November 6, 2003',
+    currentVersion: '35',
+    distroLogo:
+      'https://upload.wikimedia.org/wikipedia/commons/3/3f/Fedora_logo.svg',
+    officialWebsite: 'https://getfedora.org/',
   },
   {
-    name: 'martha',
-    text: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
+    distroName: 'Debian',
+    description:
+      'One of the oldest and most stable Linux distributions, known for its commitment to free and open-source software.',
+    firstRelease: 'August 16, 1993',
+    currentVersion: '11 (Bullseye)',
+    distroLogo: 'https://www.debian.org/Pics/debian-logo-1024x576.png',
+    officialWebsite: 'https://www.debian.org/',
   },
   {
-    name: 'john',
-    text: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
+    distroName: 'Arch Linux',
+    description:
+      'A lightweight and flexible Linux distribution that follows the KISS principle (Keep It Simple, Stupid).',
+    firstRelease: 'March 11, 2002',
+    currentVersion: 'Rolling Release',
+    distroLogo:
+      'https://upload.wikimedia.org/wikipedia/commons/e/e8/Archlinux-logo-standard-version.png',
+    officialWebsite: 'https://archlinux.org/',
+  },
+  {
+    distroName: 'CentOS',
+    description:
+      'A community-supported Linux distribution derived from sources freely provided to the public by Red Hat.',
+    firstRelease: 'May 2004',
+    currentVersion: '8 (Stream)',
+    distroLogo:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Centos-logo-light.svg/1024px-Centos-logo-light.svg.png',
+    officialWebsite: 'https://www.centos.org/',
   },
 ];
 
-export { data };
+export { linuxDistros };
